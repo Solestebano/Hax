0890f67e36b1ca6c979d6837e63ad42027917758
unknown <solestebano102743@gmail.com>
2025-06-12 03:13:07 -0500

    Mover logica del contador desde GameManager a TimerManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,59 +1,12 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
-using DG.Tweening;
 using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour{
-
-    [SerializeField] TMP_Text texto_contador;
-    [SerializeField] float duracion;
-    [SerializeField] float duracion_interpolacion;
-    private float duracion_actual;
-    private bool empezo_animacion = false;
-    private bool primer_movimiento = false;
-    private bool esta_pausado = false;
-    public bool ganar = false;
-
-
     private void Awake(){
         Application.targetFrameRate = 60;
-        duracion_actual = duracion;
-
-    }
-
-    private void Update(){
-        //Condicion para ganar
-        if (duracion_actual == 0 && !ganar){
-            ganar = true;
-
-        }
-
-        //Contador
-        if(duracion_actual < 0) {
-            duracion_actual = 0;
-        
-        }else if (duracion_actual != 0 && primer_movimiento){
-            duracion_actual -= Time.deltaTime;
-        
-        }
-
-        
-        //Cambio de color del texto cuando queda poco tiempo
-        if(duracion_actual < 30 && !empezo_animacion){
-            texto_contador.DOColor(Color.red, duracion_interpolacion)
-            .SetLoops(-1, LoopType.Yoyo)
-            .SetEase(Ease.Linear);
-
-            empezo_animacion = true;
-        
-        }
-
-        //Actualizacion del contador
-        TimeSpan tiempo = TimeSpan.FromSeconds(duracion_actual);
-        texto_contador.text = tiempo.ToString(@"mm\:ss");
 
     }
 
@@ -61,27 +14,5 @@ public class GameManager : MonoBehaviour{
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
 
     }
-    public void EmpezarContador(){
-        primer_movimiento = true;
-    
-    }
-
-    public void PararContador(bool pausa){
-        primer_movimiento = false;
-        esta_pausado = pausa;
-
-    }
-
-    public bool GetPrimerMovimiento(){
-        return primer_movimiento;
-    
-    }
-
-    public bool GetPausa(){
-        return esta_pausado;
-    
-    }
-
-
 
 }
--- Assets/Scripts/TimerManager.cs
+++ Assets/Scripts/TimerManager.cs
@@ -1,18 +1,78 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using DG.Tweening;
 
-public class TimerController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+public class TimerController : MonoBehaviour{
+
+    [SerializeField] TMP_Text texto_contador;
+    [SerializeField] float duracion_contador;
+    [SerializeField] float duracion_interpolacion;
+    private float duracion_actual;
+    private bool empezo_animacion = false;
+    private bool esta_pausado = false;
+    private bool primer_movimiento = false;
+    public bool ganar = false;
+
+    private void Awake(){
+        duracion_actual = duracion_contador;
+
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    private void Update(){
+        //Condicion para ganar
+        if (duracion_actual == 0 && !ganar){
+            ganar = true;
+
+        }
+
+        //Contador
+        if(duracion_actual < 0) {
+            duracion_actual = 0;
         
+        }else if (duracion_actual != 0 && primer_movimiento){
+            duracion_actual -= Time.deltaTime;
+        
+        }
+
+        
+        //Cambio de color del texto cuando queda poco tiempo
+        if(duracion_actual < 30 && !empezo_animacion){
+            texto_contador.DOColor(Color.red, duracion_interpolacion)
+            .SetLoops(-1, LoopType.Yoyo)
+            .SetEase(Ease.Linear);
+
+            empezo_animacion = true;
+        
+        }
+
+        //Actualizacion del contador
+        TimeSpan tiempo = TimeSpan.FromSeconds(duracion_actual);
+        texto_contador.text = tiempo.ToString(@"mm\:ss");
+
     }
+
+    public void EmpezarContador(){
+        primer_movimiento = true;
+    
+    }
+
+    public void PararContador(bool pausa){
+        primer_movimiento = false;
+        esta_pausado = pausa;
+
+    }
+
+    public bool GetPrimerMovimiento(){
+        return primer_movimiento;
+    
+    }
+
+    public bool GetPausa(){
+        return esta_pausado;
+
+    }
+
 }
