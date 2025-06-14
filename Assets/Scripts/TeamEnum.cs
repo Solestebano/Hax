ce76c550af4683bcaaf485f40295d5e29305ed6b
unknown <solestebano102743@gmail.com>
2025-06-14 07:41:40 -0500

    Añadir enum en TeamEnum

--- Assets/Scripts/TeamEnum.cs
+++ Assets/Scripts/TeamEnum.cs
@@ -2,17 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TeamEnum : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+public class TeamEnum : MonoBehaviour{
+    public enum Team{
+        Team1,
+        Team2
+    
+    } 
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
